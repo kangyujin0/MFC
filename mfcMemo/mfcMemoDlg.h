0e928827388e6c320196ca2bcf07ba68cf73dbe7
kangyujin0 <149138977+kangyujin0@users.noreply.github.com>
2024-01-09 10:11:40 +0900

    메모장_main mfcMemoDlg.h

--- mfcMemo/mfcMemoDlg.h
+++ mfcMemo/mfcMemoDlg.h
@@ -6,17 +6,17 @@
 
 
 // CmfcMemoDlg 대화 상자
-class CmfcMemoDlg : public CDialogEx					// main class이므로 변수를 private
+class CmfcMemoDlg : public CDialogEx						// main class이므로 변수를 private
 {
 	CString sFind, sReplace;
 	CString Data = "";
 	int start_pos = 0;
-	int mEncoding = 0;									// 0:ANSI  1:UTF-8
+	int mEncoding = 0;							// 0:ANSI  1:UTF-8
 	int fntSize;
 	CString fntName;
 
 public:
-	CmfcMemoDlg(CWnd* pParent = nullptr);				// 표준 생성자입니다.
+	CmfcMemoDlg(CWnd* pParent = nullptr);					// 표준 생성자입니다.
 	
 
 // 대화 상자 데이터입니다.
@@ -25,14 +25,14 @@ public:
 #endif
 
 	protected:		
-	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+	virtual void DoDataExchange(CDataExchange* pDX);			// DDX/DDV 지원입니다.
 
 
 // 구현입니다.
 protected:
 	HICON m_hIcon;
-	HACCEL mAccel;										// Accelerator를 가리키는 변수
-	CEdit mEditMemo;									// 객체화
+	HACCEL mAccel;								// Accelerator를 가리키는 변수
+	CEdit mEditMemo;							// 객체화
 
 	// 생성된 메시지 맵 함수
 	virtual BOOL OnInitDialog();
@@ -41,7 +41,7 @@ protected:
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
 public:
-	CStatusBarCtrl mStatusBar;							// 상태표시줄
+	CStatusBarCtrl mStatusBar;						// 상태표시줄
 	afx_msg void OnMenuOpen();
 	afx_msg void OnMenuAbout();
 	afx_msg void OnMenuFind();
